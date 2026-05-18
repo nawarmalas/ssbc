4b1764d69e54627b86d7439800d84adaf56df25d
Rafik <r.aita@ligadata.com>
2026-05-18 14:44:50 +0300

    fix: add HasFactory to User, create UserFactory, set valid APP_KEY in phpunit.xml

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
