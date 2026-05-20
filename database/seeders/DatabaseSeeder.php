af20361c5842087f87ed8231912443f22c22f5b6
Nawar Malas <nawar3179@gmail.com>
2026-05-20 13:28:06 +0300

    Add manifest.json for asset management in ssbc-build

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -41,8 +41,12 @@ class DatabaseSeeder extends Seeder
                 'contact_phone' => '+966 11 000 0000',
                 'address_en' => 'Riyadh, Kingdom of Saudi Arabia',
                 'address_ar' => 'الرياض، المملكة العربية السعودية',
-                'linkedin_url' => null,
-                'twitter_url' => null,
+                'social_links' => [
+                    'linkedin' => null,
+                    'x' => null,
+                    'instagram' => null,
+                    'facebook' => null,
+                ],
                 'footer_desc_en' => 'The Syrian Saudi Business Council is a formal bilateral institution dedicated to building enduring economic ties between the Syrian and Saudi business communities.',
                 'footer_desc_ar' => 'مجلس الأعمال السوري السعودي هو مؤسسة ثنائية رسمية مكرسة لبناء روابط اقتصادية دائمة بين مجتمعَي الأعمال السوري والسعودي.',
             ]);
